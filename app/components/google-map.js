75477a9689d5bad56816fe5dbbe2e595e8266c96
Patrick Dougall <pdougall1@gmail.com>
2015-03-25 00:05:16 -0400

    refactor google map

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -2,14 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['google-map'],
+  hugeLat: 33.792418, // should really get passed in
+  hugeLng: -84.385605, // should really get passed in
+
+  staticOptions: {
+    zoom: 8,
+    scrollwheel: false,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false
+  },
+
+  mapOptions: function () {
+    var options = this.get('staticOptions');
+    options.center = new google.maps.LatLng(this.get('hugeLat'), this.get('hugeLng'));
+    return options;
+  }.property(),
+
+  map: function () {
+    var element = document.getElementById(this.get('elementId'));
+    return new google.maps.Map(element, this.get('mapOptions'));
+  }.property(),
 
   initialize: function () {
-    var mapOptions = {
-      zoom: 8,
-      center: new google.maps.LatLng(-34.397, 150.644)
-    };
-    var map = new google.maps.Map(document.getElementById(this.get('elementId')), mapOptions);
-    this.set('map', map);
+    this.set('map', this.get('map'));
   }.on('didInsertElement')
 
 });
