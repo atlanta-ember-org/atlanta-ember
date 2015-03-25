51d53c91ee1fc7e9ea78837d7abfdbf01bb7df93
Patrick Dougall <pdougall1@gmail.com>
2015-03-24 23:04:24 -0400

    Google maps is just that easy right
      * No it is not, for some reason the API key isn't quite working :(
      * This is popping up in the console.
        - InvalidKeyOrUnauthorizedURLMapError: https://developers.google.com/maps/documentation/javascript/tutorial#api_key

---
+++ app/components/google-map.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['google-map'],
+
+  initialize: function () {
+    var mapOptions = {
+      zoom: 8,
+      center: new google.maps.LatLng(-34.397, 150.644)
+    }
+    var map = new google.maps.Map(document.getElementById(this.get('elementId')), mapOptions);
+    this.set('map', map);
+  }.on('didInsertElement')
+
+});
