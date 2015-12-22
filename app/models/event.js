20b9e960a116d03a71d22c676e615d51673b7997
Patrick Dougall <pdougall1@gmail.com>
2015-12-22 16:45:15 -0500

    Whitespace

--- app/models/event.js
+++ app/models/event.js
@@ -1,13 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string', { defaultValue: '' }),
-  description: DS.attr('string', { defaultValue: '' }),
+  name:                DS.attr('string', { defaultValue: '' }),
+  description:         DS.attr('string', { defaultValue: '' }),
   originallyCreatedAt: DS.attr('date'),
   yesRsvpCount:        DS.attr('number'),
   startsAt:            DS.attr('date'),
   meetupId:            DS.attr('string'),
   eventUrl:            DS.attr('string'),
   venue:               DS.belongsTo('venue'),
-  topics: DS.hasMany('topic')
+  topics:              DS.hasMany('topic')
 });
