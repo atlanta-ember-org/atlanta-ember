79a7a2ada49345727bdd36b98ccf66498bf75f87
Patrick Dougall <pdougall1@gmail.com>
2015-12-28 17:05:47 -0500

    Adds a topics list

--- mirage/config.js
+++ mirage/config.js
@@ -1,5 +1,7 @@
 export default function() {
 
+  this.get('http://localhost:3000/topics');
+
   // These comments are here to help you get started. Feel free to delete them.
 
   /*
--- tests/acceptance/topics-test.js
+++ tests/acceptance/topics-test.js
@@ -1,6 +1,18 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'atlantaember/tests/helpers/module-for-acceptance';
 
+const doesInclude = function (string, substrings) {
+  const statuses = substrings.map(function (substring) {
+    if(string.indexOf(substring) > -1) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  return statuses.indexOf(false) < 0;
+};
+
 moduleForAcceptance('Acceptance | topic');
 
 test('visiting /topics', function(assert) {
@@ -16,5 +28,18 @@ test('has a title', function(assert) {
 
   andThen(function() {
     assert.equal(this.$('h1.page-title').text().trim(), 'Topics');
+  });
+});
+
+test('has a list of topics', function(assert) {
+  visit('/topics');
+  const topicNames = server.createList('topic', 3)
+    .map(function (topic) {
+      return topic.name;
+    });
+
+  andThen(function() {
+    let string = this.$('.topics li').text();
+    assert.ok(doesInclude(string, topicNames));
   });
 });
