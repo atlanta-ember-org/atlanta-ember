ae7bc4ab191546df71b0f3ec3814e6ec971ed2a4
Chris McCuller <cmcculler@salesforce.com>
2016-01-06 21:46:33 -0500

    Add ember-cli-mirage to enable local development without needing the rails API

--- config/environment.js
+++ config/environment.js
@@ -32,6 +32,7 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     ENV.baseURL = '/';
+    ENV.domain = '';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
