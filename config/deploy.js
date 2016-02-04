e53e32099d9d244c0561b648fd20fa139cf16824
Chris McCuller <cmcculler@salesforce.com>
2016-02-04 13:24:59 -0500

    Simplify deployment config

--- config/deploy.js
+++ config/deploy.js
@@ -17,22 +17,10 @@ module.exports = function(deployTarget) {
     }
   };
 
-  if (deployTarget === 'staging') {
-    ENV.build.environment = 'staging';
-    ENV.s3.bucket = process.env.S3_BUCKET;
-    ENV.s3.region = process.env.S3_REGION;
-    ENV.cloudfront.distribution = process.env.CF_DISTRIBUTION;
-  }
+  ENV.build.environment = deployTarget;
+  ENV.s3.bucket = process.env.S3_BUCKET;
+  ENV.s3.region = process.env.S3_REGION;
+  ENV.cloudfront.distribution = process.env.CF_DISTRIBUTION;
 
-  if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    ENV.s3.bucket = process.env.S3_BUCKET;
-    ENV.s3.region = process.env.S3_REGION;
-    ENV.cloudfront.distribution = process.env.CF_DISTRIBUTION;
-  }
-
-  // Note: if you need to build some configuration asynchronously, you can return
-  // a promise that resolves with the ENV object instead of returning the
-  // ENV object synchronously.
   return ENV;
 };
