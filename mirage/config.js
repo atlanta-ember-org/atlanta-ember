5f0987d1114c924b4caa43ec9be35d32996b0388
Chris McCuller <cmcculler@salesforce.com>
2016-07-29 13:23:10 -0400

    Setup new mirage

--- mirage/config.js
+++ mirage/config.js
@@ -1,27 +1,14 @@
 export default function() {
 
-  this.get('http://localhost:3000/topics');
-  this.post('http://localhost:3000/topics');
+  this.get('/events', (schema) => {
+    return schema.events.all();
+  });
 
-  // These comments are here to help you get started. Feel free to delete them.
+  this.get('/topics', (schema) => {
+    return schema.topics.all();
+  });
 
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-  */
+  this.post('/topics', (schema, request) => {
+    return { name: request.params.name };
+  });
 }
