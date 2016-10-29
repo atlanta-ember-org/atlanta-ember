9644caa137d73016e2aba5ef6b9e4476c939a6ea
Chris McCuller <cmcculler@salesforce.com>
2016-10-28 23:47:10 -0400

    naming conventions

--- app/models/venue.js
+++ app/models/venue.js
@@ -5,10 +5,10 @@ import { hasMany } from 'ember-data/relationships';
 export default Model.extend({
   country:   attr('string'),
   city:      attr('string'),
-  address_1: attr('string'),
+  address1: attr('string'),
   name:      attr('string'),
   lat:       attr('number'),
   lon:       attr('number'),
   repinned:  attr('boolean'),
-  event:     hasMany('event')
+  events:     hasMany('event')
 });
